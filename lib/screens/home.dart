10d27ea1fef95267086347edd7171d370f09d6c0
raminBgrn <ramin.bagheriian@gmail.com>
2023-05-30 13:10:25 +0330

    fist commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("images/ramin-bagherian.jpg"),
+          ),
+        ),
+        child: const Row(children: [
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Text(
+                  'Hello',
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.amber,
+                  ),
+                ),
+                Text(
+                  "I'm Ramin Bagherian",
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.amber,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: SizedBox(),
+          )
+        ]),
+      ),
+    );
+  }
+}
